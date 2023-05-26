b1b953790049495efe3ca2db12fdc0ba1766566b
Keyur Khant <keyur.khant@edba.io>
2023-05-26 14:05:50 -0300

    feat: inital frontend setup

---
+++ trivia-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <header>
+        Multi-cloud Trivia Game
+      </header>
+    </div>
+  );
+}
---
+++ trivia-frontend/src/components/game-lobby/index.tsx
@@ -0,0 +1,2 @@
+// This file contains logic of Trivia Game Lobby
+export {};
