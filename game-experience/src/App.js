d07ecf7a12f2b5b35c087be1d7c46eefd12ec7df
HVMS <viralsid2330@gmail.com>
2023-06-01 23:08:49 -0300

    App java script file updated

--- game-experience/src/App.js
+++ game-experience/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
+import { ChakraProvider, Center, Button} from '@chakra-ui/react';
+import AddQuestionPage from './pages/AddQuestionPage';
 
 function App() {
+
+  const HomePage = () => {
+    return (
+      <Center mt={8}>
+        <Button colorScheme="blue" as={Link} to="/addquiz" mr={4}>
+          Add Question
+        </Button>
+        <Button colorScheme="blue" as={Link} to="/editquiz" ml={4}>
+          Edit Question
+        </Button>
+        <Button colorScheme="blue" as={Link} to="/deletequiz" ml={4}>
+          Delete Question
+        </Button>
+      </Center>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/addquiz" element={<AddQuestionPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+
   );
 }
 
